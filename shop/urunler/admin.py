b188c69b0601d5a94d87a0e464be7c746062de17
Habip <114939978+habipokc@users.noreply.github.com>
2026-02-10 14:21:20 +0300

    create model and add to admin panel

--- shop/urunler/admin.py
+++ shop/urunler/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Kategoriler
 
 # Register your models here.
+
+admin.site.register(Kategoriler)
--- shop/urunler/models.py
+++ shop/urunler/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Kategoriler(models.Model):
+    isim = models.CharField(max_length=155)
+    aktifmi = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Kategoriler'
+        verbose_name = 'Kategoriler'
+
+
+    def __str__(self):
+        return self.isim
